834b8ef9eedb0cf31c1cbf9bfdae823108675c5e
paczaj-code <marcinpaczkowski@icloud.com>
2023-03-05 09:17:57 +0100

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <h1>Hello from {{name}}!</h1>
+    <h>Hello from duspko {{name}}!</h1>
     <a target="_blank" href="https://angular.io/start">
       Learn more about Angular 
     </a>
